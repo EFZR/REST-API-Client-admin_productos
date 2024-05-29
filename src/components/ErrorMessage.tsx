fea132eed6b1fda34bcf0dbd9b9748de61b26f29
efzr <emerson.zapatarivas@gmail.com>
2024-05-29 16:58:41 -0600

    communication with the api

---
+++ src/components/ErrorMessage.tsx
@@ -0,0 +1,9 @@
+import { ReactNode } from "react";
+
+export default function ErrorMessage({ children }: { children: ReactNode }) {
+  return (
+    <div className="text-center my-4 bg-red-600 text-white font-bold uppercase p-3">
+      {children}
+    </div>
+  );
+}
